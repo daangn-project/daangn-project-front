f69c19796fc375cfd248b7afd7b792c3b41badb3
sunghyeon <josh5734@naver.com>
2022-01-11 17:20:13 +0900

    create : home comonent & router

---
+++ daangn.project/src/Home.js
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+import Main from "./Main"
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <Main />
+        </>
+    )
+}
+
+export default Home;
---
+++ daangn.project/src/Main.js
@@ -0,0 +1,15 @@
+const Main = () => {
+    return (
+        <section className="home-main-section background-pale-green">
+            <div className="home-main-content">
+                <div className="home-main-image"></div>
+                <div>
+                    <h1 className="home-main-title">지금 바로 당근해요.</h1>
+                    <p class="text-m">당근장터, 동네 커뮤니티, 채팅까지! 우리의 생활을 이웃과 함께 해요.</p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Main;
