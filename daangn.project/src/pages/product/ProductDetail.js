6febbdbfa74311a8be2cbd5797cf72c26846bfcf
sunghyeon <josh5734@naver.com>
2022-02-28 14:23:16 +0900

    change: itempost to product

--- daangn.project/src/pages/product/ProductDetail.js
+++ daangn.project/src/pages/product/ProductDetail.js
@@ -31,7 +31,7 @@ const ProductDetail = () => {
     // 게시물 상세 정보 불러오기 - itemId가 변할 때 리렌더링
     useEffect(() => {
         getPageInfo(`http://localhost:8080/products/${id}`)
-        .then(itemDetail => setOtherProduct(itemDetail.itemPostByUserDtos));
+        .then(productDetail => setOtherProduct(productDetail.productByUserDtos));
     }, [id]) 
 
     return (
